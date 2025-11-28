92222c725f7db1ff4de05ed406880d4d48b26bcd
N1K <219769234+cosmon1k@users.noreply.github.com>
2025-11-28 10:54:26 +0300

    Hotfixes

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -1,54 +1,53 @@
 const MODULE_ID = 'cs-click-to-scene';
 const SETTING_ID = {
-  ENABLE_SCENES_LIST_JUMP: 'enableScenesListJump',
+    ENABLE_SCENES_LIST_JUMP: 'enableScenesListJump',
 };
 
 Hooks.once('init', () => {
-  if (!game.modules.get('lib-wrapper')?.active) {
-    if (game.user?.isGM) ui.notifications.error(game.i18n.localize('CS.CLICK_TO_SCENE.LibWrapperError'));
-    return;
-  }
+    if (!game.modules.get('lib-wrapper')?.active) {
+        if (game.user?.isGM) ui.notifications.error(game.i18n.localize('CS.CLICK_TO_SCENE.LibWrapperError'));
+        return;
+    }
 
-  const monksSceneNavigationActive = !game.modules.get('monks-scene-navigation')?.active;
-  if (monksSceneNavigationActive && game.user?.isGM) {
-    ui.notifications.info(game.i18n.localize('CS.CLICK_TO_SCENE.MONKS_ACTIVE'));
-  }
+    const monksSceneNavigationActive = game.modules.get('monks-scene-navigation')?.active;
+    if (monksSceneNavigationActive && game.user?.isGM) {
+        ui.notifications.info(game.i18n.localize('CS.CLICK_TO_SCENE.MonksNavigationActive'));
+    }
 
-  registerSettings({
-    [SETTING_ID.ENABLE_SCENES_LIST_JUMP]: monksSceneNavigationActive,
-  });
+    registerSettings({
+        [SETTING_ID.ENABLE_SCENES_LIST_JUMP]: !monksSceneNavigationActive,
+    });
 
-  libWrapper.register(
-    MODULE_ID,
-    'Scene.prototype._onClickDocumentLink',
-    function (wrapped, event) {
-      const id = event.target?.dataset?.id;
-      const scene = game.scenes.get(id);
-      if (!scene) return wrapped(event);
+    libWrapper.register(
+        MODULE_ID,
+        'Scene.prototype._onClickDocumentLink',
+        function (wrapped, event) {
+            const id = event.target?.dataset?.id;
+            const scene = game.scenes.get(id);
+            if (!scene) return wrapped(event);
 
-      handleSceneClick(event, scene);
-    },
-    'MIXED',
-  );
+            handleSceneClick(event, scene);
+        },
+        'MIXED',
+    );
 
-  handleScenesListSettingChange(game.settings.get(MODULE_ID, SETTING_ID.ENABLE_SCENES_LIST_JUMP));
+    handleScenesListSettingChange(game.settings.get(MODULE_ID, SETTING_ID.ENABLE_SCENES_LIST_JUMP));
 });
 
 function handleSceneClick(event, scene) {
-  if (event.ctrlKey || event.metaKey) {
-    return scene.activate();
-  }
+    if (event.ctrlKey || event.metaKey) {
+        return scene.activate();
+    }
 
-  if (event.altKey) {
-    return scene.sheet.render(true);
-  }
+    if (event.altKey) {
+        return scene.sheet.render(true);
+    }
 
-  return scene.view();
+    return scene.view();
 }
 
 function handleScenesListSettingChange(enabled) {
     const cbName = 'foundry.applications.sidebar.tabs.SceneDirectory.prototype._onClickEntry';
-    libWrapper.unregister(MODULE_ID, cbName);
 
     if (enabled) {
         const getSceneId = (event) => event.target?.parentElement?.dataset?.entryId;
@@ -64,14 +63,14 @@ function handleScenesListSettingChange(enabled) {
 }
 
 function registerSettings(options) {
-  game.settings.register(MODULE_ID, SETTING_ID.ENABLE_SCENES_LIST_JUMP, {
-    name: game.i18n.localize('CS.CLICK_TO_SCENE.SETTINGS.EnableScenesList.Name'),
-    hint: game.i18n.localize('CS.CLICK_TO_SCENE.SETTINGS.EnableScenesList.Hint'),
-    scope: 'world',
-    config: true,
-    type: Boolean,
-    default: options[SETTING_ID.ENABLE_SCENES_LIST_JUMP],
-    onChange: enabled => handleScenesListSettingChange(enabled),
-  });
+    game.settings.register(MODULE_ID, SETTING_ID.ENABLE_SCENES_LIST_JUMP, {
+        name: game.i18n.localize('CS.CLICK_TO_SCENE.SETTINGS.EnableScenesList.Name'),
+        hint: game.i18n.localize('CS.CLICK_TO_SCENE.SETTINGS.EnableScenesList.Hint'),
+        scope: 'world',
+        config: true,
+        requiresReload: true,
+        type: Boolean,
+        default: options[SETTING_ID.ENABLE_SCENES_LIST_JUMP],
+    });
 }
 
